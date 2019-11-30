658bfebebe2016199c7f779ae30082879c4b5f5b
Allen Zhang <azhang9328@gmail.com>
2019-11-30 03:59:12 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -2,8 +2,9 @@ def find_element_index(array, value_to_find)
   # Add your solution here
   array.length.times do 
     if array.include?(value_to_find)
-      array.index(value_to_find)
+      puts array.index(value_to_find)
     end   
+    puts loop
   end    
 end
 
