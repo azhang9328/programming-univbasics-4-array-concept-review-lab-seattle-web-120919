b07a7c526b8fa49a8db31b5b3c6ca0365a0772c3
Allen Zhang <azhang9328@gmail.com>
2019-11-30 03:59:18 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -4,7 +4,7 @@ def find_element_index(array, value_to_find)
     if array.include?(value_to_find)
       puts array.index(value_to_find)
     end   
-    puts loop
+    puts "loop"
   end    
 end
 
